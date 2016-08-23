447884e65063c66a7c5fb8c203ef402498a2fb44
熊岑 <xiongcen@corp.netease.com>
2016-08-23 20:57:47 +0800

    练习题2

--- src/com/xiongcen/exercise/E02_Fibonacci.java
+++ src/com/xiongcen/exercise/E02_Fibonacci.java
@@ -1,19 +1,43 @@
 package com.xiongcen.exercise;
 
+import java.util.Arrays;
+
 /**
  * Created by xiongcen on 16/8/20.
  */
 public class E02_Fibonacci {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++)
+            new Thread(new Fibonacci(i)).start();
+    }
 }
 
 class Fibonacci implements Generator<Integer>, Runnable {
+
+    private int count;
+    private final int n;
+    public Fibonacci(int n) { this.n = n; }
+    private int fib(int n) {
+        if (n < 2) return 1;
+        return fib(n - 2) + fib(n - 1);
+    }
+
     @Override
     public void run() {
-
+        System.out.println("Fibonacci.run n:"+n);
+        Integer[] sequence = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            sequence[i] = next();
+            System.out.println("Fibonacci.for i:"+i);
+        }
+        System.out.println("Fibonacci.yield before n:"+n);
+//        Thread.yield();
+        System.out.println(
+                "Seq. of " + n + ": " + Arrays.toString(sequence));
     }
 
     @Override
     public Integer next() {
-        return null;
+        return fib(count++);
     }
 }
