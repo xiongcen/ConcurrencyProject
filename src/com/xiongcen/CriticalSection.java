e5df33d342f0b9f4ba363e25d5afd87917db4c8d
xiongcen <xiongcen@xiongcendeiMac.local>
2016-09-08 13:07:13 +0800

    分析说明+结果

--- src/com/xiongcen/CriticalSection.java
+++ src/com/xiongcen/CriticalSection.java
@@ -18,7 +18,14 @@ import java.util.concurrent.atomic.AtomicInteger;
  * PairManager类持有一个Pair对象并控制对它的一切访问.注意唯一的public方法是getPair(),它是synchronized的.
  * 对于抽象方法increment(),对increment()的同步控制将在实现的时候进行处理.
  *
+ * PairManipulator被创建用来测试两种不同类型的PairManager,其方法是在某个任务中调用increment(),
+ * 而PairChecker则在另一个任务中执行.为了跟踪可以运行测试的频度,PairChecker在每次成功时都递增checkCounter.
  *
+ * 尽管每次运行的结果可能会非常不同,但一般来说,对于PairChecker的检查频率,
+ * PairManager1.increment()不允许有PairManger2.increment()那样多.
+ * 因为PairManger2.increment()采用同步控制块进行同步,所以对象不加锁的时间更长.
+ *
+ * !!!所以使用同步控制块而不是对整个方法进行同步控制的原因:使得其他线程能更多地访问(在安全的情况下尽可能多)!!!
  *
  * Created by xiongcen on 16/9/5.
  */
@@ -92,6 +99,7 @@ abstract class PairManager {
 
 class PairManager1 extends PairManager {
 
+    /** synchronized关键字不属于方法特征签名的组成部分,所以可以在覆盖方法的时候加上去 */
     @Override
     public synchronized void increment() {
         p.incrementX();
@@ -110,6 +118,7 @@ class PairManager2 extends PairManager {
             p.incrementY();
             temp = getPair();
         }
+        /** store()方法将一个Pair对象添加到synchronized ArrayList中,所以操作线程安全 */
         store(temp);
     }
 }
@@ -176,4 +185,7 @@ public class CriticalSection {
                 pman2 = new PairManager2();
         testApproaches(pman1, pman2);
     }
-}
+}/* Output:
+pm1: Pair: x:41,y:41 checkCounter = 3
+pm2: Pair: x:41,y:41 checkCounter = 102456413
+*///:~
