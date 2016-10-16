21ced2e6da4bbd7bcdb6301bf1f7c4ab66e63467
xiongcen <442655182@qq.com>
2016-10-16 14:22:53 +0800

    验证在通过java.util.concurrent.locks.Lock接口加上锁后,线程状态不是进入BLOCKED状态,而是WAITING状态，因为Lock接口对于阻塞的实现使用了LockSupport类中的相关方法,具体参考Java线程状态变迁图

--- src/com/xiongcen/art/ThreadState.java
+++ src/com/xiongcen/art/ThreadState.java
@@ -1,23 +1,30 @@
 package com.xiongcen.art;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Java并发编程艺术 4.1.4线程的状态 代码清单4-3
  * 使用终端:1.jps 2.jstack 进程id 查看线程状态
  * Created by xiongcen on 16/10/15.
  */
 public class ThreadState {
+
+    private static final ReentrantLock lock = new ReentrantLock();
+
     public static void main(String[] args) {
         new Thread(new Running(), "RunningThread").start();
         new Thread(new TimeWaiting(), "TimeWaitingThread").start();
         new Thread(new Waiting(), "WaitingThread").start();
         new Thread(new Blocked(), "BlockedThread-1").start();
         new Thread(new Blocked(), "BlockedThread-2").start();
+        new Thread(new Locked(), "LockedThread-1").start();
+        new Thread(new Locked(), "LockedThread-2").start();
     }
 
     static class Running implements Runnable {
         @Override
         public void run() {
-            while(true) {
+            while (true) {
                 System.out.println("Running.run");
             }
         }
@@ -27,7 +34,7 @@ public class ThreadState {
     static class TimeWaiting implements Runnable {
         @Override
         public void run() {
-            while(true) {
+            while (true) {
                 SleepUtils.second(100);
             }
         }
@@ -57,6 +64,17 @@ public class ThreadState {
                 while (true) {
                     SleepUtils.second(100);
                 }
+            }
+        }
+    }
+
+    // 该线程在通过java.util.concurrent.locks.Lock接口加上锁后,线程状态不是进入BLOCKED状态,而是WAITING状态
+    // 因为Lock接口对于阻塞的实现使用了LockSupport类中的相关方法,具体参考Java线程状态变迁图
+    static class Locked implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                lock.lock();
             }
         }
     }
