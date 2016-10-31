dfdba29af5f1b0ae29674f88fb0ddd70b65935df
xiongcen <xiongcen@xiongcendeiMac.local>
2016-10-31 20:30:14 +0800

    补充内容

--- src/com/xiongcen/art/seven/AtomicIntegerTest.java
+++ src/com/xiongcen/art/seven/AtomicIntegerTest.java
@@ -1,7 +1,16 @@
 package com.xiongcen.art.seven;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by xiongcen on 16/10/31.
  */
 public class AtomicIntegerTest {
+    public static void main(String[] args) {
+        int a = 1;
+        AtomicInteger ai = new AtomicInteger(a);
+        System.out.println("ai.getAndIncrement()=" + ai.getAndIncrement());
+        System.out.println("ai.get()=" + ai.get());
+        System.out.println("a=" + a);
+    }
 }
