774ebf31355713b8414325126183e8bf96e3c64d
xiongcen <442655182@qq.com>
2016-10-31 23:38:16 +0800

    补充说明CyclicBarrier构造方法参数传入错误会造成的问题

--- src/com/xiongcen/art/eight/CyclicBarrierTest.java
+++ src/com/xiongcen/art/eight/CyclicBarrierTest.java
@@ -47,4 +47,8 @@ public class CyclicBarrierTest {
 }
 /**
  * 主线程子线程的调度由CPU决定,两个线程都有可能先执行,所以会产生两种输出.
+ * <p/>
+ * <p/>
+ * 如果把new CyclicBarrier(2)修改成new CyclicBarrier(3),则主线程和子线程会永远等待,
+ * 因为没有第三个线程执行await()方法,即没有第三个线程到达屏障,所以之前到达屏障的线程都不会继续执行.
  */
